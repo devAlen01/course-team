fbcb42f257db8ff3de20bed6a22257087ff48730
7chyngyz <sultanmuratovchyngyz@gmail.com7>
2025-01-08 16:39:55 +0600

    cards finish

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from "react";
 import Section1 from "./Home/Section1";
 import Section2 from "./Home/Section2";
+import Section3 from "./Home/Section3";
 
 const Home: FC = () => {
   return <>
     <Section1 />
     <Section2 />
+    <Section3 />
   </>;
 };
 
